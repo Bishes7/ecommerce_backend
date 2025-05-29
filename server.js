b080a88b758fe4f359deb94e145c84ddafffb2d4
Bishes Adhikari <specialbishes9@gmail.com>
2025-05-29 15:48:17 +1000

    backend server created and made live

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import products from "./data/products.js";
+
+const PORT = 8000;
+
+const app = express();
+
+// get method
+app.get("/", (req, res) => {
+  res.send("server is live");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+// find the products from id
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+// listening the port
+app.listen(PORT, (error) =>
+  error
+    ? console.log(error)
+    : console.log(`Server is running at http://localhost:${PORT}`)
+);
