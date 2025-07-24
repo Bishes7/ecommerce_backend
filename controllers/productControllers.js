5ec902d1cf45ef06664af34dd95fff91773092a8
Bishes Adhikari <specialbishes9@gmail.com>
2025-07-24 13:18:59 +1000

    edit products route and controller made and tested

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -15,3 +15,44 @@ export const getProduct = asyncHandler(async (req, res, next) => {
     throw new Error("Product not found");
   }
 });
+
+// create Product  - Admin Only
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new productModel({
+    name: "Sample name",
+    price: 0,
+    user: req.userInfo._id,
+    image: "/image/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// Update Product - Admin Only
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await productModel.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(200).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,20 @@
 import express from "express";
 import {
+  createProduct,
   getAllProducts,
   getProduct,
+  updateProduct,
 } from "../controllers/productControllers.js";
+import { admin, isAuthenticated } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
 
 router.get("/:id", getProduct);
+
+router.post("/", isAuthenticated, admin, createProduct);
+
+router.put("/:id", isAuthenticated, admin, updateProduct);
 
 export default router;
