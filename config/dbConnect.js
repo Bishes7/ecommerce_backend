1a6b0c2d02ef2c5fb7dead84737daabf843ed667
Bishes Adhikari <specialbishes9@gmail.com>
2025-09-21 00:49:44 +1000

    code refactored for pproduction

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URL);
-    console.log("MongoDB is connected");
+    const uri = process.env.MONGODB_URL || process.env.MONGO_URL;
+    if (!uri) throw new Error("Mongo URI not set in env variables");
+
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
